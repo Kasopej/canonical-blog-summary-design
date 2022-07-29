87d4e161fac3314d8974217985e8e38e7f620225
Kasope Johnson <kasopej@gmail.com>
2022-07-29 14:38:13 +0100

    renders author name & blog title

--- src/components/BlogPostSummary.vue
+++ src/components/BlogPostSummary.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="p-card">
+  <div class="p-card p-card--highlighted branded-border">
+    <div class="p-card__content u-no-padding--top" style="font-size: 0.9rem">
+      {{ blog._embedded["wp:term"][1][0].name.toUpperCase() }}
+    </div>
+    <hr />
     <div class="p-card__content">
       <img
         class="p-card__image"
@@ -8,11 +12,13 @@
         width="330"
         :src="blog.featured_media"
       />
-      <h4>
-        <a href="#">{{ blog.title.rendered }}</a>
-      </h4>
-      <p class="u-no-padding--bottom">
-        By <a class="text-primary">{{ blog._embedded.author[0].name }}</a> on
+      <div class="fix-height">
+        <a class="text-primary p-heading--3">{{ blog.title.rendered }}</a>
+      </div>
+      <p class="author">
+        By
+        <a class="text-primary">{{ blog._embedded.author[0].name }}</a>
+        on
         {{ formattedDate }}
       </p>
     </div>
